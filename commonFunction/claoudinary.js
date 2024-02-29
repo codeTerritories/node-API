c68ee6017caf822b74ab9956c66a4bdf6f583e6f
codeTerritories <113830981+codeTerritories@users.noreply.github.com>
2024-02-29 17:54:21 +0530

    Update claoudinary.js

--- commonFunction/claoudinary.js
+++ commonFunction/claoudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: "ddyanby11",
-    api_key: "938126834644282",
-    api_secret: "0uErcwihZG_XO_yv9RZ8Kg40YPI"
+    cloud_name: "xxxxxx",
+    api_key: "xxxxxx",
+    api_secret: "xxxxxxx"
   });
 
 module.exports = cloudinary;
